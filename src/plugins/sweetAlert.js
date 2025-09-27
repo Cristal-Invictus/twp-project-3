17becfa0257fecf80000e58b6437ee20a0e3938c
Cristal-Invictus <cristal.akoyessou@epitech.eu>
2025-09-27 23:32:31 +0100

    Modification du fichier gitignore

--- src/plugins/sweetAlert.js
+++
@@ -1,3 +0,0 @@
-// Plugin SweetAlert obsolète : remplacé par les imports directs 'import Swal from "sweetalert2"'
-// Ce fichier est conservé vide pour éviter des erreurs d'import résiduelles éventuelles.
-export default { install() {} };
